694442e66050125732c603ca7661d4f94e5c57d8
Salokhiddin Rakhmonkulov <33337801+Infinity10K@users.noreply.github.com>
2020-09-09 13:40:22 +0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'afaf4F8S6Ffe4f89w4f984ds84f89w4d86a8f48wq4f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://smrmachines.herokuapp.com/']
 
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite3.db',
+        'NAME': os.path.join('BASE_DIR' , 'db.sqlite3'),
     }
 }
 
