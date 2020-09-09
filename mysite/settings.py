76ba060ea9e9d49c60234632621fdcb9741c6132
Salokhiddin Rakhmonkulov <infinity10k@ya.ru>
2020-09-09 13:43:38 +0700

    adada

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,10 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/static/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
